ac6671b4b786f87f90340e3025742a0366f8405a
Bentxi <rbtx13@gmail.com>
2024-09-07 15:58:49 -0500

    defined pages, components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //     { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //     { path: 'home', component: DashboardComponent , children: [ // 4200/home
+  //         {path: "", redirectTo; 'users', pathMatch:'full'}, // 4200/home/users
+  //         {path: 'users', component: ListComponent} // 4200/home/users y en el html: cardComponent
+  //         {path: 'user/:id', component: ViewComponent} //
+  //         {path: 'new-user', component: FormComponent} //
+  //         {path: 'update-user/:id', component: FormComponent} //
+  //     ]
+  // }
+  //     { path: '**', redirectTo: 'home'},
+];
+
+/*
+entonces, necesitamoscrear pages de:
+dashboard
+list
+view
+form
+y un componente de card
+
+
+*/
