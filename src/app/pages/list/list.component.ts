52d3c276649609476df0ddda23dc8dc6fdc910de
Bentxi <rbtx13@gmail.com>
2024-09-07 20:21:35 -0500

    listTS: array filling correctly, listHTML: @for appcard

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  usersService = inject(UsersService);
 
+  respObj: any = {}
+  arrUsers: IUser[] = [];
+
+  async ngOnInit() {
+    try {
+      const response = await this.usersService.getAll();
+      this.respObj = response;
+      this.arrUsers = this.respObj.results
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
